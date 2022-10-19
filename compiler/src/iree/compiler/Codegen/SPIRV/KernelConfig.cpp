7b17796e9ab7a7c7d61e18c1bf22850d4b5f037f
Thomas Raoux <thomasraoux@google.com>
2022-10-19 16:30:04 +0000

    Enable softmax fusion through aggressive fusion

--- compiler/src/iree/compiler/Codegen/SPIRV/KernelConfig.cpp
+++ compiler/src/iree/compiler/Codegen/SPIRV/KernelConfig.cpp
@@ -1065,6 +1065,7 @@ LogicalResult initSPIRVLaunchConfig(ModuleOp module) {
             "unhandled multiple roots in dispatch region");
       }
       rootOperation = computeOp;
+      break;
     }
 
     if (!rootOperation) {
