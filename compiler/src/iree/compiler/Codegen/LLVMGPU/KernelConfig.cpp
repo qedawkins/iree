a8b7c1b09100d30f66c78d435839a0a5c250dde1
stanley-nod <stanley@nod-labs.com>
2022-10-25 10:41:58 -0700

    [spirv] Enable Fp16 warp reduction
    [spirv] Tile conv fp16 to vectorize to 8xf16

--- compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/KernelConfig.cpp
@@ -505,6 +505,15 @@ static LogicalResult setWarpReductionConfig(func::FuncOp entryPoint,
     groupSize = llvm::APIntOps::GreatestCommonDivisor(
                     {64, uint64_t(groupSize)}, {64, uint64_t(maxWorkgroupSize)})
                     .getZExtValue();
+
+    // Current warp reduction pattern is a two step butterfly warp reduce.
+    // First, do warp reductions along multiple warps.
+    // Second, reduce results from multiple subgroups using single warp reduce.
+    // The final warp reduce requires numWarp > cudaWarpSize to work.
+    // TODO: Add flexible num steps of warp reduce to handle more tile/dist.
+    if (groupSize / cudaWarpSize > cudaWarpSize) {
+      return failure();
+    }
   }
   std::array<int64_t, 3> workgroupSize = {groupSize, 1, 1};
   SmallVector<unsigned> partitionedLoops =
