76a602fd38117566bd25710301f0d7cf9e7202e2
Quinn Dawkins <quinn@nod-labs.com>
2023-08-01 18:44:28 -0400

    Drop RematerializeParallelOps

--- compiler/src/iree/compiler/Codegen/Common/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/Common/Passes.cpp
@@ -18,7 +18,7 @@ void addCommonTargetExecutablePreprocessingPasses(OpPassManager &passManager) {
   passManager.addNestedPass<func::FuncOp>(
       IREE::LinalgExt::createDecomposeSoftmaxPass());
   // Temporary solution to avoid large allocations due to softmax lowering.
-  passManager.addNestedPass<func::FuncOp>(createRematerializeParallelOpsPass());
+  //passManager.addNestedPass<func::FuncOp>(createRematerializeParallelOpsPass());
 }
 
 //===---------------------------------------------------------------------===//
