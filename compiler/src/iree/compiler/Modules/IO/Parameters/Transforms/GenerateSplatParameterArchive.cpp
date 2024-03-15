50ab3e7b65a3e73b3768401660f18ee42a9cfaa3
Quinn Dawkins <quinn@nod-labs.com>
2024-03-15 15:57:46 -0400

    [IO] Add flag for splatting the full constant size

--- compiler/src/iree/compiler/Modules/IO/Parameters/Transforms/GenerateSplatParameterArchive.cpp
+++ compiler/src/iree/compiler/Modules/IO/Parameters/Transforms/GenerateSplatParameterArchive.cpp
@@ -26,6 +26,11 @@ namespace mlir::iree_compiler::IREE::IO::Parameters {
 #define GEN_PASS_DEF_GENERATESPLATPARAMETERARCHIVEPASS
 #include "iree/compiler/Modules/IO/Parameters/Transforms/Passes.h.inc"
 
+static llvm::cl::opt<bool> clUseFullSplatParameters(
+    "iree-io-use-full-splat-parameters",
+    llvm::cl::desc("Includes the full splatted constant in the archive"),
+    llvm::cl::init(false));
+
 namespace {
 
 struct GenerateSplatParameterArchivePass
@@ -53,6 +58,7 @@ struct GenerateSplatParameterArchivePass
     });
 
     bool hasParameter = false;
+    SmallVector<IREE::Util::GlobalOp> fullGlobals;
     // Walk the globals in the module.
     for (auto global : moduleOp.getOps<IREE::Util::GlobalOp>()) {
       // Look for globals backed by parameters.
@@ -73,17 +79,36 @@ struct GenerateSplatParameterArchivePass
       iree_io_physical_size_t storageSize = parameterAttr.getStorageSize();
 
       // Add a zero-splat entry to the builder for this global.
-      char c0 = 0;
-      iree_status_t status = iree_io_parameter_archive_builder_add_splat_entry(
-          &builder,
-          iree_string_view_t{
-              parameterName.data(),
-              static_cast<iree_host_size_t>(parameterName.size())},
-          /*metadata=*/iree_const_byte_span_empty(),
-          /*pattern=*/&c0, /*pattern_length=*/1, /*data_length=*/storageSize);
-      if (failed(handleRuntimeError(moduleOp, status,
-                                    "Failed to add splate entry for global"))) {
-        return signalPassFailure();
+      if (clUseFullSplatParameters) {
+        // Add a data entry to the builder for this global instead.
+        iree_status_t status = iree_io_parameter_archive_builder_add_data_entry(
+            &builder,
+            iree_string_view_t{
+                parameterName.data(),
+                static_cast<iree_host_size_t>(parameterName.size())},
+            /*metadata=*/iree_const_byte_span_empty(),
+            /*alignment=*/IREE_IO_PARAMETER_ARCHIVE_DEFAULT_DATA_ALIGNMENT,
+            storageSize);
+        if (failed(handleRuntimeError(moduleOp, status,
+                                      "Failed to add data entry for global"))) {
+          return signalPassFailure();
+        }
+        fullGlobals.push_back(global);
+      } else {
+        char c0 = 0;
+        iree_status_t status =
+            iree_io_parameter_archive_builder_add_splat_entry(
+                &builder,
+                iree_string_view_t{
+                    parameterName.data(),
+                    static_cast<iree_host_size_t>(parameterName.size())},
+                /*metadata=*/iree_const_byte_span_empty(),
+                /*pattern=*/&c0, /*pattern_length=*/1,
+                /*data_length=*/storageSize);
+        if (failed(handleRuntimeError(
+                moduleOp, status, "Failed to add splat entry for global"))) {
+          return signalPassFailure();
+        }
       }
       hasParameter = true;
     }
@@ -122,6 +147,61 @@ struct GenerateSplatParameterArchivePass
       iree_io_parameter_index_release(built_index);
       iree_io_file_handle_release(target_file_handle);
     });
+
+    if (clUseFullSplatParameters) {
+      iree_io_stream_ostream llvm_stream(target_stream);
+
+      // Write all of the global contents to the appropriate data storage
+      // segments.
+      for (auto constantGlobal : fullGlobals) {
+        auto initialValueAttr = constantGlobal.getInitialValueAttr();
+        if (!initialValueAttr) {
+          continue;
+        }
+        auto parameterAttr =
+            dyn_cast<IREE::Stream::NamedParameterAttr>(initialValueAttr);
+        if (!parameterAttr) {
+          continue;
+        }
+
+        // Note that the scope is not a part of the parameter archive. If the
+        // module includes multiple scopes, multiple copies of the splat archive
+        // would need to be passed in with all possible scopes.
+        std::string parameterName = parameterAttr.getKey().str();
+
+        const iree_io_parameter_index_entry_t *target_entry = NULL;
+        iree_status_t status = iree_io_parameter_index_lookup(
+            built_index,
+            iree_string_view_t{
+                parameterName.data(),
+                static_cast<iree_host_size_t>(parameterName.size())},
+            &target_entry);
+        if (failed(handleRuntimeError(
+                moduleOp, status,
+                "Failed to write parameter index header to output file"))) {
+          return signalPassFailure();
+        }
+        status = iree_io_stream_seek(target_stream, IREE_IO_STREAM_SEEK_SET,
+                                     target_entry->storage.file.offset);
+        if (failed(handleRuntimeError(
+                moduleOp, status,
+                "Failed to seek to location of global in index"))) {
+          return signalPassFailure();
+        }
+
+        Util::BytePatternAttr serializableAttr = Util::BytePatternAttr::get(
+            constantGlobal->getContext(), parameterAttr.getType(), 0);
+
+        if (failed(serializableAttr.serializeToStream(constantGlobal.getLoc(),
+                                                      llvm::endianness::native,
+                                                      llvm_stream))) {
+          moduleOp.emitError()
+              << "Failed to serialize global " << constantGlobal;
+          return signalPassFailure();
+        }
+        llvm_stream.flush();
+      }
+    }
 
     // Commit the written file.
     llvm::Error maybeCommit = fileBuffer->commit();
